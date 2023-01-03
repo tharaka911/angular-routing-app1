e2f45db4ac441418f528efa42eceea24829d071e
tharaka911 <tharakahitibandara@gmail.com>
2023-01-03 15:43:22 +0530

    added click event handler and navigate by Url and navigate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private router : Router) {}
+
   title = 'angular-routing-app1';
+
+  navigateToTheLinkA(link: string) {
+    this.router.navigateByUrl(link).then(e=>{console.log(e)});
+  }
+
+  navigateToTheLinkB(link: string) {
+    this.router.navigate([link]).then(e=>{console.log(e)});
+  }
 }
