d9128194e0551c274a78ba304422e6a28526ba27
tharaka911 <tharakahitibandara@gmail.com>
2023-01-03 12:46:32 +0530

    router outlet is added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CustomerComponent} from "./components/customer/customer.component";
+import {ItemComponent} from "./components/item/item.component";
+import {OrderComponent} from "./components/order/order.component";
+import {PlaceOrderComponent} from "./components/place-order/place-order.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'customer', component:CustomerComponent},
+  {path:'item', component:ItemComponent},
+  {path:'order', component:OrderComponent},
+  {path:'place-order', component:PlaceOrderComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { ItemComponent } from './components/item/item.component';
+import { OrderComponent } from './components/order/order.component';
+import { PlaceOrderComponent } from './components/place-order/place-order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    ItemComponent,
+    OrderComponent,
+    PlaceOrderComponent
   ],
   imports: [
     BrowserModule,
